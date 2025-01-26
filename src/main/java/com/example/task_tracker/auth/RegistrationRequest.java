b8f6b90ed6c2ab667ba5bba3dce8afb8e977947c
elaa <elaa.brahmii@gmail.com>
2025-01-26 16:23:31 +0100

    made attributes equal to User's attributes

--- src/main/java/com/example/task_tracker/auth/RegistrationRequest.java
+++ src/main/java/com/example/task_tracker/auth/RegistrationRequest.java
@@ -5,21 +5,23 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@Data
 public class RegistrationRequest {
 
 
     @NotEmpty(message = "Firstname is mandatory")
     @NotNull(message = "Firstname is mandatory")
-    private String firstname;
+    private String firstName;
     @NotEmpty(message = "Lastname is mandatory")
     @NotNull(message = "Lastname is mandatory")
-    private String lastname;
+    private String lastName;
     @Email(message = "Email is not well formatted")
     @NotEmpty(message = "Email is mandatory")
     @NotNull(message = "Email is mandatory")
