2a75430a1561b25268a67a6e6e05718a411d296e
elaa <elaa.brahmii@gmail.com>
2025-02-11 12:33:25 +0100

    add valid token method

--- frontend/src/app/services/interceptor/http-token.interceptor.ts
+++ frontend/src/app/services/interceptor/http-token.interceptor.ts
@@ -8,17 +8,19 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TokenService } from '../token/token.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpTokenInterceptor implements HttpInterceptor {
 
   constructor(
-    private tokenService:TokenService
+    private tokenService:TokenService,private router:Router
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token=this.tokenService.token;
     if(token){
+      if(this.tokenService.isTokenValid()){
       //inject my authorization header inside my request
       const authReq=request.clone({
         headers:new HttpHeaders({
@@ -29,6 +31,10 @@ export class HttpTokenInterceptor implements HttpInterceptor {
 
 
     }
+    else{
+      this.tokenService.clearToken();
+      this.router.navigate(['/login']);
+    }}
     return next.handle(request);
   }
 }
